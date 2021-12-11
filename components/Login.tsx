b6b402bad84852e559b945943fc09e3c5b6f3980
Collin Schaafsma <schaafsma@gmail.com>
2021-12-10 22:31:12 -0700

    connect to metamask and lookup address

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,8 +1,26 @@
 import { Center, Button } from '@chakra-ui/react'
+import { ethers } from 'ethers'
 
 const Login = () => {
-  const doLogin = () => {
-    console.log('Do Login')
+  const doLogin = async () => {
+    //eslint-disable-next-line
+    const metamask = window.ethereum
+    
+    if (typeof metamask === undefined) throw Error('No metamask')
+
+    await metamask.request({
+      method: 'eth_requestAccounts',
+    })
+
+    let provider: ethers.providers.Web3Provider = new ethers.providers.Web3Provider(metamask)
+    const [address] = await provider.listAccounts()
+
+    if (!address) throw Error('No address')
+
+    let ens: string | null = await provider.lookupAddress(address)
+
+    if (ens) console.log(ens)
+
   }
 
   return (
