f335a3e77d19e09de73db08fbde762885ace6f56
Collin Schaafsma <schaafsma@gmail.com>
2021-12-14 18:51:17 -0700

    logout endpoint

---
+++ pages/api/logout.ts
@@ -0,0 +1,17 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { withIronSessionApiRoute } from 'iron-session/next'
+import { sessionOptions } from '../../lib/session'
+
+type ResponseData = {
+  loggedIn: boolean
+}
+
+const handler = (
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) => {
+  req.session.destroy()
+  res.status(205).json({loggedIn: false})
+}
+
+export default withIronSessionApiRoute(handler, sessionOptions)
